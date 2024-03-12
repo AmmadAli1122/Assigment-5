796711125590516e06b542c34346e7747cc588bf
Ammad Ali <aaammadali376@gmail.com>
2024-03-12 08:07:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var quote = "Two things are infinite: the universe and human stupidity: and I am not sure about the universe";
+var famousPerson = "Albert Einstein";
+console.log("".concat(famousPerson, " once said,\"").concat(quote, "\"."));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//save quote and famousperson variable 
+const quote:string="Two things are infinite: the universe and human stupidity: and I am not sure about the universe";
+const famousPerson:string="Albert Einstein";
+//print the statment bu using template literal
+console.log( `${famousPerson} once said,"${quote}".`);
